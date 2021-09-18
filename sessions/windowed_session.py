26c08fff06f59931756a4af76188b15d260b946b
d0rj <hanaan@internet.ru>
2021-09-18 16:27:44 +0300

    refactor: some line length changes

--- sessions/windowed_session.py
+++ sessions/windowed_session.py
@@ -33,35 +33,43 @@ def _user_input(events) -> Direction:
 def _draw_map(game: Game, window):
     window.fill(_COLORS['white'])
 
+    H = 10
+
     size = game.map.size
     for i in range(0, size[0]):
-        pygame.draw.line(window, _COLORS['grey'], [i * 10, 0], [i * 10, size[1] * 10])
+        pygame.draw.line(
+            window,
+            _COLORS['grey'],
+            [i * H, 0], [i * H, size[1] * H],
+            width=3 if i in [0, size[0]] else 1
+        )
     for i in range(0, size[1]):
-        pygame.draw.line(window, _COLORS['grey'], [0, i * 10], [size[0] * 10, i * 10])
-    pygame.draw.line(window, _COLORS['black'], [0, 0], [0, size[1] * 10], width=3)
-    pygame.draw.line(window, _COLORS['black'], [size[0] * 10, 0], [size[0] * 10, size[1] * 10], width=5)
-    pygame.draw.line(window, _COLORS['black'], [0, 0], [size[0] * 10, 0], width=2)
-    pygame.draw.line(window, _COLORS['black'], [0, size[1] * 10], [size[0] * 10, size[1] * 10], width=5)
+        pygame.draw.line(
+            window,
+            _COLORS['grey'],
+            [0, i * H], [size[0] * H, i * H],
+            width=3 if i in [0, size[1]] else 1
+        )
 
     pygame.draw.rect(
         window,
         _COLORS['red'],
-        pygame.Rect(game.food_pos[0] * 10, game.food_pos[1] * 10, 10, 10)
+        pygame.Rect(game.food_pos[0] * H, game.food_pos[1] * H, H, H)
     )
     for pos in game.snake.body:
         pygame.draw.rect(window,
             _COLORS['green'],
-            pygame.Rect(pos[0] * 10, pos[1] * 10, 10, 10)
+            pygame.Rect(pos[0] * H, pos[1] * H, H, H)
         )
     pygame.draw.rect(window,
         _COLORS['dark_green'],
-        pygame.Rect(game.snake.position[0] * 10, game.snake.position[1] * 10, 10, 10)
+        pygame.Rect(game.snake.position[0] * H, game.snake.position[1] * H, H, H)
     )
     
     for pos in game.map.cells:
         pygame.draw.rect(window,
             _COLORS['black'],
-            pygame.Rect(pos[0] * 10, pos[1] * 10, 10, 10)
+            pygame.Rect(pos[0] * H, pos[1] * H, H, H)
         )
 
     pygame.display.update()
@@ -78,6 +86,7 @@ def session(ai_func: Callable[[Game], Direction], by_keyboard: bool = False):
     difficulty = 30
     frame_size_x = 720
     frame_size_y = 480
+    H = 10
 
     pygame.display.set_caption('Snake AI')
     game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
@@ -85,7 +94,7 @@ def session(ai_func: Callable[[Game], Direction], by_keyboard: bool = False):
 
     game = Game(
         Map(
-            (frame_size_x // 10, frame_size_y // 10),
+            (frame_size_x // H, frame_size_y // H),
             [(40, i) for i in range(5, 15)]
         ),
         Snake()
