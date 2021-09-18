37e93c3460e48dfd71218ebecdd0f0badf466fbe
d0rj <hanaan@internet.ru>
2021-09-18 13:44:47 +0300

    style, refactor: some style changes

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import pygame
 
 check_errors = pygame.init()
 if check_errors[1] > 0:
-    print(f'[!] Had {check_errors[1]} errors when initialising game, exiting...')
+    print(f'Had {check_errors[1]} errors when initialising game, exiting...')
     exit(-1)
 else:
-    print('[+] Game successfully initialised')
+    print('Game successfully initialised')
 
 
 COLORS: Dict[str, pygame.Color] = {
@@ -43,7 +43,8 @@ class Map:
 class Snake:
     direction: Direction
     position: Tuple[int, int] = (10, 5)
-    body: List[Tuple[int, int]] = field(default_factory=lambda: [(10, 5), (10 - 1, 5), (10 - 2, 5)])
+    body: List[Tuple[int, int]] =\
+        field(default_factory=lambda: [(10, 5), (10 - 1, 5), (10 - 2, 5)])
 
 
 @dataclass
@@ -64,30 +65,36 @@ class Game:
         )
 
     def move_snake(self):
-        if self.snake.direction == Direction.UP:
-            self.snake.position = self.snake.position[0], self.snake.position[1] - 1
-        if self.snake.direction == Direction.DOWN:
-            self.snake.position = self.snake.position[0], self.snake.position[1] + 1
-        if self.snake.direction == Direction.LEFT:
-            self.snake.position = self.snake.position[0] - 1, self.snake.position[1]
-        if self.snake.direction == Direction.RIGHT:
-            self.snake.position = self.snake.position[0] + 1, self.snake.position[1]
+        head_pos = self.snake.position
 
-        self.snake.body.insert(0, self.snake.position)
-        if self.snake.position[0] == self.food_pos[0] and self.snake.position[1] == self.food_pos[1]:
+        if self.snake.direction == Direction.UP:
+            self.snake.position = head_pos[0], head_pos[1] - 1
+        if self.snake.direction == Direction.DOWN:
+            self.snake.position = head_pos[0], head_pos[1] + 1
+        if self.snake.direction == Direction.LEFT:
+            self.snake.position = head_pos[0] - 1, head_pos[1]
+        if self.snake.direction == Direction.RIGHT:
+            self.snake.position = head_pos[0] + 1, head_pos[1]
+
+        head_pos = self.snake.position
+
+        self.snake.body.insert(0, head_pos)
+        if head_pos[0] == self.food_pos[0] and head_pos[1] == self.food_pos[1]:
             self.score += 1
             self.spawn_food()
         else:
             self.snake.body.pop()
 
     def is_snake_collides(self) -> bool:
-        if self.snake.position[0] < 0 or self.snake.position[0] > self.map.size[0] - 1:
+        head_pos = self.snake.position
+
+        if head_pos[0] < 0 or head_pos[0] > self.map.size[0] - 1:
             return True
-        if self.snake.position[1] < 0 or self.snake.position[1] > self.map.size[1] - 1:
+        if head_pos[1] < 0 or head_pos[1] > self.map.size[1] - 1:
             return True
 
         for block in self.snake.body[1:]:
-            if self.snake.position[0] == block[0] and self.snake.position[1] == block[1]:
+            if head_pos[0] == block[0] and head_pos[1] == block[1]:
                 return True
 
         return False
@@ -132,15 +139,22 @@ def user_input(events) -> Direction:
 
 def draw_map(game: Game, window):
     window.fill(COLORS['black'])
-    pygame.draw.rect(window, COLORS['white'], pygame.Rect(game.food_pos[0] * 10, game.food_pos[1] * 10, 10, 10))
+    pygame.draw.rect(
+        window,
+        COLORS['white'],
+        pygame.Rect(game.food_pos[0] * 10, game.food_pos[1] * 10, 10, 10)
+    )
     for pos in game.snake.body:
-        pygame.draw.rect(window, COLORS['green'], pygame.Rect(pos[0] * 10, pos[1] * 10, 10, 10))
+        pygame.draw.rect(window,
+            COLORS['green'],
+            pygame.Rect(pos[0] * 10, pos[1] * 10, 10, 10)
+        )
 
     pygame.display.update()
 
 
-def session(ai_function: Callable[[Game], Direction], by_keyboard: bool = False):
-    difficulty = 25
+def session(ai_func: Callable[[Game], Direction], by_keyboard: bool = False):
+    difficulty = 30
     frame_size_x = 720
     frame_size_y = 480
 
@@ -168,11 +182,11 @@ def session(ai_function: Callable[[Game], Direction], by_keyboard: bool = False)
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     pygame.event.post(pygame.event.Event(pygame.QUIT))
-        
+
         if by_keyboard:
             change_to = user_input(events) or game.snake.direction
         else:
-            change_to = ai_function(game)
+            change_to = ai_func(game)
 
         if not Direction.is_opposite(change_to, game.snake.direction):
             game.snake.direction = change_to
@@ -185,7 +199,11 @@ def session(ai_function: Callable[[Game], Direction], by_keyboard: bool = False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run snake session')
-    parser.add_argument('-k', '--keyboard', action='store_true', help='(flag) Control snake by keyboard')
+    parser.add_argument(
+        '-k', '--keyboard',
+        action='store_true',
+        help='(flag) Control snake by keyboard'
+    )
 
     args = parser.parse_args()
     
