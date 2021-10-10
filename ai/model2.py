3c48df250533d489ec5460765340cf4b66f9e670
d0rj <hanaan@internet.ru>
2021-10-11 00:54:42 +0300

    feat: add snake AI based on 2nd model
    But it retrains on every run, i can't just load pretrained model.

--- ai/model2.py
+++ ai/model2.py
@@ -1,32 +1,27 @@
-from keras.engine.sequential import Sequential
-from tensorflow import keras
 import numpy as np
 
 from engine import Game, Direction
-from nn2 import where_snake_blocked, get_angle, change_direction
+from nn2 import SnakeNN, add_action, get_game_action, generate_observation
 
 
-model: Sequential = keras.models.load_model('./models/snake_model2')
+snake_nn = SnakeNN()
+model = snake_nn.train_model(snake_nn.get_training_data(), snake_nn.model())    
 
 
 def snake_ai(game: Game) -> Direction:
     global model
-    actions = [-1, 0, 1]
 
-    snake_pos, food_pos, _, _ = game.get_info()
+    prev_observation = generate_observation(game)
+    predictions = []
+    for action in range(-1, 2):
+        input_ = add_action(prev_observation, action)
+        input_ = input_.reshape(-1, 5, 1)
+        predictions.append(model.predict(input_))
 
-    prev_observation = where_snake_blocked(game)
-    angle = get_angle(snake_pos[0], food_pos)
+    action = np.argmax(np.array(predictions))
+    new_direction = get_game_action(
+        game.snake.body,
+        action - 1
+    )
 
-    features = np.array([
-        np.append(prev_observation, [angle, -1]),
-        np.append(prev_observation, [angle, 0]),
-        np.append(prev_observation, [angle, 1])
-    ])
-
-    predictions = model.predict(features)
-    predictions = [np.argmax(i) for i in predictions]
-
-    action = actions[np.argmax(predictions)]
-
-    return change_direction(key=action, snake_direction=game.snake.direction)
+    return new_direction
